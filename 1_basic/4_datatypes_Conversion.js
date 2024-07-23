46dc99c793e1c1d2a2a16f48f5d4ed42a52304fc
Satyam7666 <104294545+Satyam7666@users.noreply.github.com>
2024-07-23 05:26:54 +0000

    conversion of datatypes

---
+++ 1_basic/4_datatypes_Conversion.js
@@ -0,0 +1,32 @@
+let score="34";
+let MyNname="Stayam"
+//console.log(typeof score);
+//console.log(typeof(MyNname));
+
+let valNumber=Number(score);
+let valName=Number(MyNname);
+
+//console.log(valNumber);
+//console.log(typeof valNumber);
+//console.log(typeof valName);
+
+// "34"=>33
+//"34abc" => nan(not a number)
+//true=> 1; false=> 0;
+
+let isLOggedin="satyam";
+
+let booleanLogedIn=Boolean(isLOggedin);
+//console.log(booleanLogedIn);
+
+//1=> true; 0=>false
+// ""=>false;
+//"satyame"=>true
+
+
+
+let someNumber=34;
+
+let isString=String(someNumber);
+console.log(someNumber);
+console.log(typeof isString);
