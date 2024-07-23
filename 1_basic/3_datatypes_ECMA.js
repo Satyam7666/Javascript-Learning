13a98bc649ba82f7f2962e1cf248b1c0c14ed3ad
Satyam7666 <104294545+Satyam7666@users.noreply.github.com>
2024-07-23 05:05:18 +0000

    data types

---
+++ 1_basic/3_datatypes_ECMA.js
@@ -0,0 +1,25 @@
+//number
+let num=34;  //ranges is from 2 to the power 53
+//STring
+let stringdata="Satyam";
+//boolean
+let x=Boolean;
+//person object
+const person={firstName: "satyam",lastName:"thorat"};
+
+//array object 
+const color=["red","yellow","blue","black"];
+
+//date object
+const date=new Date("22-04-2024");
+//data types
+//Number
+//string
+//boolean
+//object
+//undefined  => means value is not assing yet .type of undefine is undefine
+// null => standalone value , null means not a undefined. null is a object type
+//symbol=> unique
+//console.table([num,stringdata,person,color,date]);
+console.log(typeof stringdata);
+console.log(typeof date);
